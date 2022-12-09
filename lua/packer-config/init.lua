7c769b654c19d5965702e1abba0352d3bbd44b47
Roberth <45379245+roberth1927@users.noreply.github.com>
2022-12-09 15:44:19 -0500

    theme
    theme change

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -71,13 +71,13 @@ return require'packer'.startup(function(use)
     }
        -- indent line
      use 'lukas-reineke/indent-blankline.nvim'
-    
-    
+
      -- blade de laravel
     use "jwalton512/vim-blade"
 
-    -- comentar
+    -- comentar seleciona  + gc
     use "tpope/vim-commentary"
+    use "tpope/vim-obsession"
 
 
 end)
